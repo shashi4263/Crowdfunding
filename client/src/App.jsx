d6a45cb914013fe5560dd17193db627aef03abab
shashikiller <rshashi133@gmail.com>
2024-02-04 18:48:18 +0530

    created campain

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,9 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
         </Routes>
       </div>
     </div>
