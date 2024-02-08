3d4883e7f560fcd05b42835b4b21b7b023fd0a5d
shashikiller <rshashi133@gmail.com>
2024-02-08 19:18:00 +0530

    campaign show

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -7,6 +7,7 @@ import {
   useContractWrite,
 } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
+import { EditionMetadataWithOwnerOutputSchema } from "@thirdweb-dev/sdk";
 
 const StateContext = createContext();
 
@@ -26,19 +27,74 @@ export const StateContextProvider = ({ children }) => {
     try {
       const data = await createCampaign({
         args: [
-          address, //owner
-          form.title, //title
-          form.description, //description
-          from.target,
-          new Date(form.deadline).getTime(), //deadline
+          address, // owner
+          form.title, // title
+          form.description, // description
+          form.target,
+          new Date(form.deadline).getTime(), // deadline,
           form.image,
         ],
       });
+
       console.log("contract call success", data);
     } catch (error) {
       console.log("contract call failure", error);
     }
   };
+
+  const getCampaigns = async () => {
+    const campaigns = await contract.call("getCampaigns");
+
+    const parsedCampaings = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(
+        campaign.amountCollected.toString()
+      ),
+      image: campaign.image,
+      pId: i,
+    }));
+
+    return parsedCampaings;
+  };
+
+  const getUserCampaigns = async () => {
+    const allCampaigns = await getCampaigns();
+
+    const filteredCampaigns = allCampaigns.filter(
+      (campaign) => campaign.owner === address
+    );
+
+    return filteredCampaigns;
+  };
+
+  const donate = async (pId, amount) => {
+    const data = await contract.call("donateToCampaign", [pId], {
+      value: ethers.utils.parseEther(amount),
+    });
+
+    return data;
+  };
+
+  const getDonations = async (pId) => {
+    const donations = await contract.call("getDonators", [pId]);
+    const numberOfDonations = donations[0].length;
+
+    const parsedDonations = [];
+
+    for (let i = 0; i < numberOfDonations; i++) {
+      parsedDonations.push({
+        donator: donations[0][i],
+        donation: ethers.utils.formatEther(donations[1][i].toString()),
+      });
+    }
+
+    return parsedDonations;
+  };
+
   return (
     <StateContext.Provider
       value={{
@@ -46,6 +102,10 @@ export const StateContextProvider = ({ children }) => {
         contract,
         connect,
         createCampaign: publishCampaign,
+        getCampaigns,
+        getUserCampaigns,
+        donate,
+        getDonations,
       }}
     >
       {children}
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -10,7 +10,7 @@ import { checkIfImage } from "../utils";
 const CreateCampaign = () => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
-  const { createCampaign } = useStateContext;
+  const { createCampaign } = useStateContext();
   const [form, setForm] = useState({
     name: "",
     title: "",
@@ -37,28 +37,17 @@ const CreateCampaign = () => {
         setIsLoading(false);
         navigate("/");
       } else {
-        alert("Provide valid image Url");
+        alert("Provide valid image URL");
         setForm({ ...form, image: "" });
       }
     });
-
-    console.log(form);
   };
 
   return (
-    <div
-      className="bg-[#1c1c24] flex justify-center items-center
-  flex-col rounded-[10px] sm:p-10 p-4"
-    >
-      {isLoading && "Loader..."}
-      <div
-        className="flex justify-center items-center p-[16px]
-      sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]"
-      >
-        <h1
-          className="font-epilogue font-bold sm:text-[25px] text-[18px] 
-      leading-[38px] text-white "
-        >
+    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+      {isLoading && <Loader />}
+      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
+        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">
           Start a Campaign
         </h1>
       </div>
@@ -70,7 +59,7 @@ const CreateCampaign = () => {
         <div className="flex flex-wrap gap-[40px]">
           <FormField
             labelName="Your Name *"
-            placeholder="JohnDoe"
+            placeholder="John Doe"
             inputType="text"
             value={form.name}
             handleChange={(e) => handleFormFieldChange("name", e)}
@@ -86,23 +75,20 @@ const CreateCampaign = () => {
 
         <FormField
           labelName="Story *"
-          placeholder="Write a story"
+          placeholder="Write your story"
           isTextArea
           value={form.description}
           handleChange={(e) => handleFormFieldChange("description", e)}
         />
 
-        <div
-          className="w-full flex justify-start items-center p-4 bg-[#8c6dfd] 
-        h-[120px] rounded-[10px]"
-        >
+        <div className="w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[120px] rounded-[10px]">
           <img
             src={money}
             alt="money"
-            className="w-[40px] h-[40px] object-contain "
+            className="w-[40px] h-[40px] object-contain"
           />
-          <h4 className="font-epilogue font-bold text-25px text-white ml-[20px]">
-            You will get 100% of the raised amount.
+          <h4 className="font-epilogue font-bold text-[25px] text-white ml-[20px]">
+            You will get 100% of the raised amount
           </h4>
         </div>
 
@@ -131,7 +117,7 @@ const CreateCampaign = () => {
           handleChange={(e) => handleFormFieldChange("image", e)}
         />
 
-        <div className="flex justify-center items-center my-[40px] ">
+        <div className="flex justify-center items-center mt-[40px]">
           <CustomButton
             btnType="submit"
             title="Submit new campaign"
